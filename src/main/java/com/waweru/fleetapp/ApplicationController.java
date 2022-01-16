34b336d1adb1bb8740fffb95c269e61f4d7e2be8
waweru-steve <wawerusteve142@yahoo.com>
2022-01-16 19:56:45 +0300

    Setup configuration files and Bootstrap templates

---
+++ src/main/java/com/waweru/fleetapp/ApplicationController.java
@@ -0,0 +1,14 @@
+package com.waweru.fleetapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+	
+	@GetMapping("/index")
+	public String goHome() {
+		return "index";
+	}
+
+}
---
+++ src/main/java/com/waweru/fleetapp/FleetappApplication.java
@@ -0,0 +1,13 @@
+package com.waweru.fleetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetappApplication.class, args);
+	}
+
+}
